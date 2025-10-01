62e167dd8171b1b83ae201200ed2452c9dcff086
Simon <133875585+simpansoftware@users.noreply.github.com>
2025-10-01 19:32:41 +0200

    recode

--- src/fr/freemann/commands/LifeStealHelpCommand.java
+++ src/fr/freemann/commands/LifeStealHelpCommand.java
@@ -8,8 +8,8 @@ public class LifeStealHelpCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3) {
-        sender.sendMessage("§aList of the commands :");
-        sender.sendMessage("§a");
+        sender.sendMessage("Â§aList of the commands :");
+        sender.sendMessage("Â§a");
         return false;
     }
 
